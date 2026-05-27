3e911de7edebf7c38983aa2470697a94ecaa8afc
ONestoDev <ernestoo.es@gmail.com>
2026-05-27 16:56:53 -0300

    Att2705I

---
+++ Estacio/ex005.py
@@ -0,0 +1,7 @@
+nome = 'Ernesto'
+print('Olá, ' + nome + '! Prazer em te conhecer!')
+print('Qual o seu ano de nascimento? ')
+ano = int(input())
+idade = 2026 - ano
+print('{} você tem  {} anos.' .format(nome, idade))
+
